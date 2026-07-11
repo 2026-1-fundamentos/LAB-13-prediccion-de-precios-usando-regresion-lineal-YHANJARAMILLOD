de3f198320dfab0f3593d502f374222cfa3bd20d
YHANJARAMILLOD <yhjaramillod@unal.edu.co>
2026-07-10 21:26:34 -0500

    primera

--- homework/homework.py
+++ homework/homework.py
@@ -1,3 +1,101 @@
+import pandas as pd
+import os
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import GridSearchCV
+import pickle
+import gzip
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+import json
+train_df = pd.read_csv("files/input/train_data.csv.zip", compression="zip")
+test_df = pd.read_csv("files/input/test_data.csv.zip", compression="zip")
+
+train_df['Age'] = 2021 - train_df['Year'].astype(int)
+test_df['Age'] = 2021 - test_df['Year'].astype(int)
+test_df = test_df.drop(columns=['Year', 'Car_Name'])
+train_df = train_df.drop(columns=['Year', 'Car_Name'])
+
+x_train=train_df.drop(columns=['Selling_Price'])
+y_train=train_df['Selling_Price']
+x_test=test_df.drop(columns=['Selling_Price'])
+y_test=test_df['Selling_Price']
+
+columnas_categoricas = ['Fuel_Type', 'Selling_type', 'Transmission']
+columnas_numericas = [col for col in x_train.columns if col not in columnas_categoricas]
+preprocesador = ColumnTransformer(
+    transformers=[
+        ('cat', OneHotEncoder(handle_unknown='ignore'), columnas_categoricas),
+        ('num', MinMaxScaler(), columnas_numericas)
+    
+    ],
+    remainder='passthrough' # ¡Muy importante! Le dice que deje las columnas numéricas intactas
+)
+pipeline_modelo = Pipeline(steps=[
+        ("preprocessor", preprocesador),
+        ("select_k_best", SelectKBest(score_func=f_regression)),
+        ("regressor", LinearRegression())
+])
+parametros_a_probar = {
+    "select_k_best__k": [10, 12, 15],
+}
+# 2. Configuramos la Validación Cruzada y la Métrica
+optimizador = GridSearchCV(
+    estimator=pipeline_modelo,            
+    param_grid=parametros_a_probar,       
+    cv=10,                                
+    scoring='neg_mean_absolute_error',          
+    n_jobs=6,
+    verbose=3,
+    refit=True,                          
+)
+optimizador.fit(x_train, y_train)
+
+mejor_modelo = optimizador.best_estimator_
+
+ruta_carpeta = 'files/models'
+nombre_archivo = 'model.pkl.gz'
+ruta_completa = os.path.join(ruta_carpeta, nombre_archivo)
+
+#print(f"📂 Paso 3: Verificando/Creando la carpeta en '{ruta_carpeta}'...")
+os.makedirs(ruta_carpeta, exist_ok=True) # Esto hace lo mismo que tu IF pero en una sola línea limpia
+
+#print("💾 Paso 4: Escribiendo el archivo model.pkl.gz en el disco...")
+with gzip.open(ruta_completa, 'wb') as f:
+    pickle.dump(optimizador, f, protocol=4)
+
+#print("🎉 ¡PROCESO TERMINADO! El modelo se guardó y creó correctamente.")
+
+y_train_pred = mejor_modelo.predict(x_train)
+y_test_pred = mejor_modelo.predict(x_test)
+
+metrics_train = {
+    'type': 'metrics',
+    'dataset': 'train',
+    'r2': r2_score(y_train, y_train_pred),
+    'mse': mean_squared_error(y_train, y_train_pred),
+    'mad': round(mean_absolute_error(y_train, y_train_pred), 4)
+    
+}
+metrics_test = {
+    'type': 'metrics',
+    'dataset': 'test',
+    'r2': r2_score(y_test, y_test_pred),
+    'mse': mean_squared_error(y_test, y_test_pred),
+    'mad': round(mean_absolute_error(y_test, y_test_pred), 4)
+    
+}
+ruta_archivo = 'files/output/metrics.json'
+os.makedirs(os.path.dirname(ruta_archivo), exist_ok=True)
+with open(ruta_archivo, 'w', encoding='utf-8') as archivo:
+    # Escribimos el diccionario de train y un salto de línea (\n)
+    archivo.write(json.dumps(metrics_train) + '\n')
+    # Escribimos el diccionario de test y un salto de línea (\n)
+    archivo.write(json.dumps(metrics_test) + '\n')
+
 #
 # En este dataset se desea pronosticar el precio de vhiculos usados. El dataset
 # original contiene las siguientes columnas:
